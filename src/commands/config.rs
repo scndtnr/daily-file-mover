1e2ae4d23218c68a65c2353043f6297f49c56490
scndtnr <scndtnr@users.noreply.github.com>
2023-05-06 11:22:34 +0900

    add date_from_dir_path() for extract command.

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use regex::Regex;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -11,23 +12,46 @@ pub(crate) struct Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            file_prefix_date_format: "%Y%m%d_".into(),
-            dir_path_date_format: "%Y-%m/%Y-%m-%d".into(),
+            file_prefix_date_format: r"%Y%m%d_".into(),
+            dir_path_date_format: r"%Y-%m\%Y-%m-%d".into(),
         }
     }
 }
 
+impl Config {
+    pub(super) fn regex_file_prefix_date_format(&self) -> Regex {
+        let pattern = self
+            .file_prefix_date_format
+            .replace("%Y", r"\d{4}")
+            .replace("%m", r"\d{2}")
+            .replace("%d", r"\d{2}");
+        Regex::new(&pattern).expect("Fail to generate regex.")
+    }
+    pub(super) fn regex_dir_path_date_format(&self) -> Regex {
+        let pattern = self
+            .dir_path_date_format
+            .replace('\\', r"\\")
+            .replace("%Y", r"\d{4}")
+            .replace("%m", r"\d{2}")
+            .replace("%d", r"\d{2}");
+        Regex::new(&pattern).expect("Fail to generate regex.")
+    }
+}
+
+/// 既定の設定情報名称を返す
 fn default_config_name() -> (&'static str, Option<&'static str>) {
     let app_name = "daily-file-mover";
     let config_name = None;
     (app_name, config_name)
 }
 
+/// 設定情報を読み込んで `Config` 構造体を返す
 pub(crate) fn load_config() -> Config {
     let (app_name, config_name) = default_config_name();
     confy::load(app_name, config_name).expect("Fail to load config.")
 }
 
+/// 設定ファイルのパスや、設定内容を表示する
 pub(crate) fn print_config() {
     let (app_name, config_name) = default_config_name();
     let cfg_path = confy::get_configuration_file_path(app_name, config_name)
@@ -37,4 +61,28 @@ pub(crate) fn print_config() {
 
     let pwd = env::current_dir().expect("Fail to get current directory.");
     println!("Currnet Directory: {:#?}", pwd);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ファイルの日付接頭辞フォーマットを正規表現パターンに変換する() {
+        let cfg = Config::default();
+        assert_eq!(cfg.file_prefix_date_format, r"%Y%m%d_");
+        assert_eq!(
+            cfg.regex_file_prefix_date_format().as_str(),
+            r"\d{4}\d{2}\d{2}_"
+        )
+    }
+    #[test]
+    fn ディレクトリパスの日付フォーマットを正規表現パターンに変換する() {
+        let cfg = Config::default();
+        assert_eq!(cfg.dir_path_date_format, r"%Y-%m\%Y-%m-%d");
+        assert_eq!(
+            cfg.regex_dir_path_date_format().as_str(),
+            r"\d{4}-\d{2}\\\d{4}-\d{2}-\d{2}"
+        )
+    }
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,38 +3,51 @@ mod extract;
 mod pack;
 
 pub(super) use config::print_config;
+pub(super) use extract::extract_daily_files;
 pub(super) use pack::pack_daily_files;
 
 use chrono::NaiveDate;
-use regex::Regex;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
 use self::config::{load_config, Config};
 
+/// 文字列を `PathBuf` に変換する
 fn convert_to_path(path_str: &str) -> PathBuf {
     PathBuf::from_str(path_str).expect("Fail to Convert src to root directory.")
 }
 
+/// 設定情報を元に、ディレクトリパスから日付情報を生成する
+fn date_from_dir_path(path: &str, cfg: &Config) -> NaiveDate {
+    let date_regex = cfg.regex_dir_path_date_format();
+    let date_str = date_regex
+        .captures(path)
+        .expect("Fail to capture by regex")
+        .get(0)
+        .expect("Not match by date_regex.")
+        .as_str();
+    NaiveDate::parse_from_str(date_str, &cfg.dir_path_date_format)
+        .expect("Fail to convert date_str to NaiveDate.")
+}
+
+/// 設定情報を元に、ファイル名に日付接頭辞が含まれているか判定する
+fn has_date_prefix(file_name: &str, cfg: &Config) -> bool {
+    let prefix_regex = cfg.regex_file_prefix_date_format();
+    prefix_regex.is_match(file_name)
+}
+
+/// 設定情報を元に、日付ディレクトリパスを作成する
 fn generate_new_dir_path_with_date(root_dir: &Path, date: &NaiveDate, cfg: &Config) -> PathBuf {
     let dir_path_str = date.format(&cfg.dir_path_date_format).to_string();
     root_dir.join(dir_path_str)
 }
 
+/// 与えられたディレクトリとファイルから `PathBuf` を生成する
 fn generate_new_file_path(new_dir_path: &Path, file_name: &str) -> PathBuf {
     new_dir_path.join(file_name)
 }
 
-fn has_date_prefix(file_name: &str, cfg: &Config) -> bool {
-    let prefix_pattern = cfg
-        .file_prefix_date_format
-        .replace("%Y", r"\d{4}")
-        .replace("%m", r"\d{2}")
-        .replace("%d", r"\d{2}");
-    let prefix_regex = Regex::new(&prefix_pattern).expect("Fail to generate regex.");
-    prefix_regex.is_match(file_name)
-}
-
+/// 設定情報を元に、日付接頭辞を持つファイルの `PathBuf` を生成する
 fn generate_new_file_path_with_date(
     new_dir_path: &Path,
     file_name: &str,
@@ -56,7 +69,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn 日付接頭辞を持っている() {
+    fn ディレクトリパスから日付情報を生成する() {
+        let path = "C:\\dev\\sandbox\\2023-05\\2023-05-04";
+        let cfg = load_config();
+
+        let date = date_from_dir_path(path, &cfg);
+        let expect_date = NaiveDate::parse_from_str("20230504", "%Y%m%d").unwrap();
+        assert_eq!(date, expect_date);
+    }
+
+    #[test]
+    fn ファイル名に日付接頭辞を持っている() {
         // 下記設定を前提とする
         // file_prefix_date_format: "%Y%m%d_"
         let file_name = "20230401_test.txt";
@@ -64,7 +87,7 @@ mod tests {
         assert!(has_date_prefix(file_name, &cfg))
     }
     #[test]
-    fn 日付接頭辞を持っていない() {
+    fn ファイル名に日付接頭辞を持っていない() {
         // 下記設定を前提とする
         // file_prefix_date_format: "%Y%m%d_"
         let file_name = "test.txt";
@@ -73,13 +96,12 @@ mod tests {
     }
 
     #[test]
-    fn 日付接頭辞を持つのでリネームされない() {
+    fn ファイル名に日付接頭辞を持つのでリネームされない() {
         // 下記設定を前提とする
         // file_prefix_date_format: "%Y%m%d_"
         let new_dir_path = convert_to_path("C:\\dev\\sandbox");
         let file_name = "20230501_test.txt";
         let date = NaiveDate::parse_from_str("20230401", "%Y%m%d").unwrap();
-        println!("{}", date);
         let cfg = load_config();
 
         // ファイルパス作成
@@ -89,7 +111,7 @@ mod tests {
     }
 
     #[test]
-    fn 日付接頭辞を持たないのでリネームされる() {
+    fn ファイル名に日付接頭辞を持たないのでリネームされる() {
         // 下記設定を前提とする
         // file_prefix_date_format: "%Y%m%d_"
         let new_dir_path = convert_to_path("C:\\dev\\sandbox");
